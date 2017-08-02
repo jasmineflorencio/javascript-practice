50840bc9e2ef93ec36391b818c00f8a0c39de522
Jasmine <jasmineflorencio@gmail.com>
2017-08-02 16:03:15 -0700

    start basic html, css, and js for color game

---
+++ color-game/script.js
@@ -0,0 +1,50 @@
+var colors = generateColors(6);
+var squares = document.querySelectorAll(".square");
+var pickedColor = pickColor();
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.getElementById("message");
+var h1 = document.querySelector("h1");
+var resetButton = document.getElementById("reset");
+
+colorDisplay.textContent = pickedColor;
+
+for (var i = 0; i < squares.length; i++) {
+  squares[i].style.backgroundColor = colors[i];
+  squares[i].addEventListener("click", function(){
+    var clickedColor = this.style.backgroundColor;
+    if(clickedColor === pickedColor){
+      messageDisplay.textContent = "Correct!";
+      changeColors(clickedColor);
+      h1.style.backgroundColor = clickedColor;
+    } else {
+      this.style.backgroundColor = "#232323";
+      messageDisplay.textContent = "Try Again!";
+    }
+  });
+}
+
+function changeColors(color){
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = color;
+  }
+}
+
+function pickColor(){
+  var random = Math.floor(Math.random() * colors.length);
+  return colors[random];
+}
+
+function generateColors(num) {
+  var arr = [];
+    for (var i = 0; i < num; i++) {
+      arr.push(randomColor());
+    }
+  return arr;
+}
+
+function randomColor() {
+  var r = Math.floor(Math.random() * 255 + 1);
+  var g = Math.floor(Math.random() * 255 + 1);
+  var b = Math.floor(Math.random() * 255 + 1);
+  return "rgb(" + r + ", " + g + ", " + b + ")";
+}
